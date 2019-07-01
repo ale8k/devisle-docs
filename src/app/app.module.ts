30c7dd570ca0464b4a63bd52cdc866e93b0b4a67
ale8k <alexanderkilroy@outlook.com>
2019-07-01 20:43:32 +0100

    task/buid-basic-comps: Add login comp
    Added login comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( { } );
   ],
   providers: [],
   bootstrap: [AppComponent]
